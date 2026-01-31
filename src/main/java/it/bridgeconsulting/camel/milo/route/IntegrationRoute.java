e220a31b8f7876441ea08b6190ddc12afb76287d
Francesco Spampinato <fspampinato@bridgeconsulting.it>
2026-01-31 23:31:54 +0100

    first commit

---
+++ src/main/java/it/bridgeconsulting/camel/milo/route/IntegrationRoute.java
@@ -0,0 +1,33 @@
+package it.bridgeconsulting.camel.milo.route;
+
+import java.util.Arrays;
+
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+import org.apache.camel.component.milo.MiloConstants;
+import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IntegrationRoute extends EndpointRouteBuilder {
+
+	public static final String TIMER_POLLER = "timerPoller";
+	
+	@Override
+	public void configure() throws Exception {
+		from(timer(TIMER_POLLER)
+			.period("1000"))
+			.routeId(TIMER_POLLER)
+			.setHeader(MiloConstants.HEADER_NODE_IDS, constant(Arrays.asList(
+                "ns=3;i=1002"
+            )))
+			.setHeader(MiloConstants.HEADER_AWAIT, constant(true))
+			.enrich(miloClient("opc.tcp://localhost:53530/OPCUA/SimulationServer")
+					.allowedSecurityPolicies(SecurityPolicy.Basic256Sha256.name())
+					.keyStoreUrl("classpath:keystore.jks")
+					.keyStorePassword("12345678")
+					.keyAlias("client1")
+					.keyPassword("12345678")
+					.applicationUri("urn:myorg:opcua:my-app"))
+			.log(">> Message from OPCUA ${body}");
+	}
+}
