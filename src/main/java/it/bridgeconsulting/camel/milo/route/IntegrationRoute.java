9c3a1e6faf6409e991bda80aa754e689e37e4cb7
Francesco Spampinato <fspampinato@bridgeconsulting.it>
2026-02-01 00:50:03 +0100

    fix format

--- src/main/java/it/bridgeconsulting/camel/milo/route/IntegrationRoute.java
+++ src/main/java/it/bridgeconsulting/camel/milo/route/IntegrationRoute.java
@@ -17,17 +17,15 @@ public class IntegrationRoute extends EndpointRouteBuilder {
 		from(timer(TIMER_POLLER)
 			.period("1000"))
 			.routeId(TIMER_POLLER)
-			.setHeader(MiloConstants.HEADER_NODE_IDS, constant(Arrays.asList(
-                "ns=3;i=1002"
-            )))
+			.setHeader(MiloConstants.HEADER_NODE_IDS, constant(Arrays.asList("ns=3;i=1002")))
 			.setHeader(MiloConstants.HEADER_AWAIT, constant(true))
 			.enrich(miloClient("opc.tcp://localhost:53530/OPCUA/SimulationServer")
-					.allowedSecurityPolicies(SecurityPolicy.Basic256Sha256.name())
-					.keyStoreUrl("classpath:keystore.jks")
-					.keyStorePassword("12345678")
-					.keyAlias("client1")
-					.keyPassword("12345678")
-					.applicationUri("urn:myorg:opcua:my-app"))
+				.allowedSecurityPolicies(SecurityPolicy.Basic256Sha256.name())
+				.keyStoreUrl("classpath:keystore.jks")
+				.keyStorePassword("12345678")
+				.keyAlias("client1")
+				.keyPassword("12345678")
+				.applicationUri("urn:myorg:opcua:my-app"))
 			.log(">> Message from OPCUA ${body}");
 	}
 }
