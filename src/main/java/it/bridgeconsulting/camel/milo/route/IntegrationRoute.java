55b33e242d95d097404a77ccb1218cedf789236b
Francesco Spampinato <fspampinato@bridgeconsulting.it>
2026-02-01 09:35:24 +0100

    configured opc plc

--- src/main/java/it/bridgeconsulting/camel/milo/route/IntegrationRoute.java
+++ src/main/java/it/bridgeconsulting/camel/milo/route/IntegrationRoute.java
@@ -11,19 +11,20 @@ import org.springframework.stereotype.Component;
 public class IntegrationRoute extends EndpointRouteBuilder {
 
 	public static final String TIMER_POLLER = "timerPoller";
+	private static final String NODE_ID = "nsu=http://microsoft.com/Opc/OpcPlc/;s=RandomSignedInt32";
 	
 	@Override
 	public void configure() throws Exception {
 		from(timer(TIMER_POLLER)
 			.period("1000"))
 			.routeId(TIMER_POLLER)
-			.setHeader(MiloConstants.HEADER_NODE_IDS, constant(Arrays.asList("ns=3;i=1002")))
+			.setHeader(MiloConstants.HEADER_NODE_IDS, constant(Arrays.asList(NODE_ID)))
 			.setHeader(MiloConstants.HEADER_AWAIT, constant(true))
-			.enrich(miloClient("opc.tcp://localhost:53530/OPCUA/SimulationServer")
+			.enrich(miloClient("opc.tcp://localhost:50000")
+				.overrideHost(true)
 				.allowedSecurityPolicies(SecurityPolicy.Basic256Sha256.name())
-				.keyStoreUrl("classpath:keystore.jks")
+				.keyStoreUrl("classpath:user.jks")
 				.keyStorePassword("12345678")
-				.keyAlias("client1")
 				.keyPassword("12345678")
 				.applicationUri("urn:myorg:opcua:my-app"))
 			.log(">> Message from OPCUA ${body}");
