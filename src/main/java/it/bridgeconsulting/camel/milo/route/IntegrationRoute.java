7013f3dcc5182345ef7d725466f87f5cbbde108d
Francesco Spampinato <fspampinato@bridgeconsulting.it>
2026-02-01 20:18:11 +0100

    code refactor

--- src/main/java/it/bridgeconsulting/camel/milo/route/IntegrationRoute.java
+++ src/main/java/it/bridgeconsulting/camel/milo/route/IntegrationRoute.java
@@ -1,32 +1,25 @@
 package it.bridgeconsulting.camel.milo.route;
 
-import java.util.Arrays;
-
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
-import org.apache.camel.component.milo.MiloConstants;
 import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IntegrationRoute extends EndpointRouteBuilder {
 
-	public static final String TIMER_POLLER = "timerPoller";
+	public static final String OPCUA_CONSUMER_ROUTE = "opcUaConsumerRoute";
 	private static final String NODE_ID = "nsu=http://microsoft.com/Opc/OpcPlc/;s=RandomSignedInt32";
 	
 	@Override
 	public void configure() throws Exception {
-		from(timer(TIMER_POLLER)
-			.period("1000"))
-			.routeId(TIMER_POLLER)
-			.setHeader(MiloConstants.HEADER_NODE_IDS, constant(Arrays.asList(NODE_ID)))
-			.setHeader(MiloConstants.HEADER_AWAIT, constant(true))
-			.enrich(miloClient("opc.tcp://localhost:50000")
+		from(miloClient("opc.tcp://localhost:50000")
 				.overrideHost(true)
 				.allowedSecurityPolicies(SecurityPolicy.Basic256Sha256.name())
 				.keyStoreUrl("classpath:user.jks")
 				.keyStorePassword("12345678")
 				.keyPassword("12345678")
-				.applicationUri("urn:myorg:opcua:my-app"))
+				.node(NODE_ID))
+			.routeId(OPCUA_CONSUMER_ROUTE)
 			.log(">> Message from OPCUA ${body}");
 	}
 }
